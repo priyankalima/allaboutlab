4e17c092b39546ec7ce4806153d9a297645f011d
priyankalima <priyankalima99@gmail.com>
2024-08-01 18:01:57 +0530

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,14 +8,14 @@ main.append(
             <div class="slider-btn" id="sliderControl">
             <button class="leftBtn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" fill="none" viewBox="0 0 41 40">
-                <path fill="#ACAFAE" d="M26.118 10.44c.245.282.382.665.382 1.063 0 .399-.137.781-.382 1.063l-6.465 7.443 6.465 7.443c.238.283.37.663.366 1.057-.003.395-.14.772-.382 1.05-.242.28-.57.438-.913.44a1.22 1.22 0 0 1-.918-.421l-7.389-8.506a1.626 1.626 0 0 1-.382-1.063c0-.399.137-.781.382-1.063l7.389-8.506c.245-.282.577-.44.923-.44.347 0 .679.158.924.44Z"/>
-                <rect width="39" height="39" x="1" y=".5" stroke="#ACAFAE" rx="9.5"/>
+                <rect class="left-rect" width="39" height="39" x="1" y=".5" stroke="#ACAFAE" rx="9.5"/>
+                <path class="left-path" fill="#ACAFAE" d="M26.118 10.44c.245.282.382.665.382 1.063 0 .399-.137.781-.382 1.063l-6.465 7.443 6.465 7.443c.238.283.37.663.366 1.057-.003.395-.14.772-.382 1.05-.242.28-.57.438-.913.44a1.22 1.22 0 0 1-.918-.421l-7.389-8.506a1.626 1.626 0 0 1-.382-1.063c0-.399.137-.781.382-1.063l7.389-8.506c.245-.282.577-.44.923-.44.347 0 .679.158.924.44Z"/>
                 </svg>                                   
             </button>
             <button class="rightBtn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" fill="none" viewBox="0 0 41 40">
-                <rect width="39" height="39" x="1" y=".5"  stroke="#ACAFAE" rx="9.5"/>
-                <path fill="#fff" d="M16.882 10.44a1.626 1.626 0 0 0-.382 1.063c0 .399.137.781.382 1.063l6.465 7.443-6.465 7.443c-.238.283-.37.663-.366 1.057.003.395.14.772.382 1.05.242.28.57.438.913.44a1.22 1.22 0 0 0 .918-.421l7.389-8.506c.245-.282.382-.664.382-1.063 0-.399-.137-.781-.382-1.063l-7.389-8.506a1.226 1.226 0 0 0-.923-.44c-.347 0-.679.158-.924.44Z"/>
+                <rect class="right-rect"  width="39" height="39" x="1" y=".5" fill="#8EC15B"  stroke="#ACAFAE" rx="9.5"/>
+                <path class="right-path" fill="#fff" d="M16.882 10.44a1.626 1.626 0 0 0-.382 1.063c0 .399.137.781.382 1.063l6.465 7.443-6.465 7.443c-.238.283-.37.663-.366 1.057.003.395.14.772.382 1.05.242.28.57.438.913.44a1.22 1.22 0 0 0 .918-.421l7.389-8.506c.245-.282.382-.664.382-1.063 0-.399-.137-.781-.382-1.063l-7.389-8.506a1.226 1.226 0 0 0-.923-.44c-.347 0-.679.158-.924.44Z"/>
                 </svg>                                   
             </button>
             </div>
@@ -42,15 +42,33 @@ main.append(
                         const carousel = document.getElementById('bannerContainer');
                         console.log(slider.offsetWidth)
                         const width = slider.offsetWidth;
-                        const maxWidth = slider.scrollLeft - slider.clientWidth;
-                        console.log(maxWidth)
-                        console.log(width)
+                        const maxWidth = carousel.scrollWidth;
+                        const scrollLeft = maxWidth - width;
+                        // const result = scrollLeft - 1;
+                        // console.log(maxWidth- slider.clientWidth)
+                        console.log( maxWidth-width)
+                        // console.log(carousel.scrollLeft)
                         document.querySelector('.leftBtn').addEventListener('click',()=>{
                             carousel.scrollBy(-width,0)
-                           
+                            console.log(carousel.scrollLeft)
+                            if(carousel.scrollLeft <= 0) {
+                                document.querySelector('.left-rect').style.fill = "transparent";
+                                document.querySelector('.right-rect').style.fill = "#8EC15B";
+                                document.querySelector('.right-path').style.fill = "#ffffff";
+                                document.querySelector('.left-path').style.fill = "#ACAFAE";
+                            }
+                            
                         })
                         document.querySelector('.rightBtn').addEventListener('click',()=>{
                             carousel.scrollBy(width,0)
+                            // console.log(maxWidth)
+                            console.log(carousel.scrollLeft)
+                            if(carousel.scrollLeft >= (scrollLeft-1)) {
+                                document.querySelector('.right-rect').style.fill = "transparent";
+                                document.querySelector('.left-rect').style.fill = "#8EC15B";
+                                document.querySelector('.right-path').style.fill = "#ACAFAE";
+                                document.querySelector('.left-path').style.fill = "#fff";
+                            }
                            
                         })
                         

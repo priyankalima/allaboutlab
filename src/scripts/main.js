f87d02ddae042e5ea66d97ac926655df06c81f49
priyankalima <priyankalima99@gmail.com>
2024-08-02 11:14:13 +0530

    commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,13 +8,13 @@ main.append(
             <div class="slider-btn" id="sliderControl">
             <button class="leftBtn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" fill="none" viewBox="0 0 41 40">
-                <rect class="left-rect" width="39" height="39" x="1" y=".5" stroke="#ACAFAE" rx="9.5"/>
+                <rect class="left-rect" width="39" height="39" x="1" y=".5" stroke="#8EC15B" rx="9.5"/>
                 <path class="left-path" fill="#ACAFAE" d="M26.118 10.44c.245.282.382.665.382 1.063 0 .399-.137.781-.382 1.063l-6.465 7.443 6.465 7.443c.238.283.37.663.366 1.057-.003.395-.14.772-.382 1.05-.242.28-.57.438-.913.44a1.22 1.22 0 0 1-.918-.421l-7.389-8.506a1.626 1.626 0 0 1-.382-1.063c0-.399.137-.781.382-1.063l7.389-8.506c.245-.282.577-.44.923-.44.347 0 .679.158.924.44Z"/>
                 </svg>                                   
             </button>
             <button class="rightBtn">
                 <svg xmlns="http://www.w3.org/2000/svg" width="41" height="40" fill="none" viewBox="0 0 41 40">
-                <rect class="right-rect"  width="39" height="39" x="1" y=".5" fill="#8EC15B"  stroke="#ACAFAE" rx="9.5"/>
+                <rect class="right-rect"  width="39" height="39" x="1" y=".5" fill="#8EC15B"  stroke="#8EC15B" rx="9.5"/>
                 <path class="right-path" fill="#fff" d="M16.882 10.44a1.626 1.626 0 0 0-.382 1.063c0 .399.137.781.382 1.063l6.465 7.443-6.465 7.443c-.238.283-.37.663-.366 1.057.003.395.14.772.382 1.05.242.28.57.438.913.44a1.22 1.22 0 0 0 .918-.421l7.389-8.506c.245-.282.382-.664.382-1.063 0-.399-.137-.781-.382-1.063l-7.389-8.506a1.226 1.226 0 0 0-.923-.44c-.347 0-.679.158-.924.44Z"/>
                 </svg>                                   
             </button>
@@ -54,6 +54,7 @@ main.append(
                             if(carousel.scrollLeft <= 0) {
                                 document.querySelector('.left-rect').style.fill = "transparent";
                                 document.querySelector('.right-rect').style.fill = "#8EC15B";
+                                 document.querySelector('.right-rect').style.stroke = "#8EC15B";
                                 document.querySelector('.right-path').style.fill = "#ffffff";
                                 document.querySelector('.left-path').style.fill = "#ACAFAE";
                             }
@@ -66,6 +67,7 @@ main.append(
                             if(carousel.scrollLeft >= (scrollLeft-1)) {
                                 document.querySelector('.right-rect').style.fill = "transparent";
                                 document.querySelector('.left-rect').style.fill = "#8EC15B";
+                                document.querySelector('.left-rect').style.stroke = "#8EC15B";
                                 document.querySelector('.right-path').style.fill = "#ACAFAE";
                                 document.querySelector('.left-path').style.fill = "#fff";
                             }
